1cd60b2e68d19202917b2563474b945c66267f9d
metheok <studentinok@gmail.com>
2020-08-04 21:21:10 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,23 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&family=Open+Sans&family=Playfair+Display&display=swap");
+/* font-family: 'Montserrat', sans-serif;
+font-family: 'Open Sans', sans-serif;
+font-family: 'Playfair Display', serif; */
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Playfair Display", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f1e3e3;
 }
-
 #nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  background-color: #f1e3e3;
+  padding: 15px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <first />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import first from "@/components/first.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    first,
+  },
 };
 </script>
+
+<style scoped>
+.home {
+}
+</style>
